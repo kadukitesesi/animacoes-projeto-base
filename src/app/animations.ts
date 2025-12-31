eb643cb9f2c1021e7e6cee03aa9b11ff401cde00
kadukitesesi <kadukitesesi@gmail.com>
2025-12-31 14:39:09 -0300

    first commit

---
+++ src/app/animations.ts
@@ -0,0 +1,45 @@
+import { style, trigger, state, transition, animate } from '@angular/animations';
+
+export const highlitedStateTrigger = trigger('highlightedState',[
+    state('default', style({
+      border: '2px solid #B2B6FF'
+    })),
+    state('highlited', style({
+      border: '4px solid #B2B6FF',
+      filter: 'brightness(92%)'
+    })),
+    transition('default => highlited',[
+      animate('200ms ease-out',
+        style({
+        transform: 'scale(1.02)'
+      }),),
+      animate(200)
+    ])
+  ])
+
+  export const shownStateTrigger = trigger('shownState', [
+
+    transition(':enter',[
+      style({
+        opacity:0
+      }),
+      animate(300, style({
+        opacity: 1
+      }))
+    ]),
+    transition(':leave',[
+      animate(300, style({
+        opacity: 0
+      }))
+    ])
+  ])
+
+  export const animateCheckBox = trigger('animateCheck', [
+    transition('* => checked',[
+      animate('400ms ease-in',
+          style({
+        transform: 'scale(0.4)'
+      }),),
+      animate(200)
+      ])
+    ])
